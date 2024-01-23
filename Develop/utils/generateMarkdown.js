63d9617c809090dda71c061d2bad2e8fcebb1c9f
zachc <zach.chapman24@gmail.com>
2024-01-23 08:17:04 -0500

    Fixed layout

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,5 +1,3 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
 function renderLicenseBadge(license) {
   if (license === "Unlicensed") {
     return "";
@@ -8,8 +6,6 @@ function renderLicenseBadge(license) {
   return `![badge](https://img.shields.io/badge/license-${license}-brightgreen)`;
 }
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
 function renderLicenseLink(license) {
   if (license === "Unlicensed") {
     return "";
@@ -17,8 +13,6 @@ function renderLicenseLink(license) {
   return "- [License](#license) ";
 }
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
 function renderLicenseSection(license) {
   if (license === "Unlicensed") {
     return "";
@@ -28,19 +22,21 @@ function renderLicenseSection(license) {
  This project is is covered under the ${license} license.`;
 }
 
-// TODO: Create a function to generate markdown for README
+
 function generateMarkdown(data) {
   return `# ${data.name} ${renderLicenseBadge(data.license)}
 
   ## Description
   ${data.descriptionProject}
   
-  ## Table of Contents (Optional)
+  ## Table of Contents
   
   - [Installation](#installation)
   - [Usage](#usage)
   - [Credits](#credits)
   ${renderLicenseLink(data.license)}
+  - [Tests](#tests)
+  - [Contribute](#contribute)
   
   ## Installation
   ${data.descriptionInstallation}
@@ -50,16 +46,11 @@ function generateMarkdown(data) {
   ${data.usage}
   
   
-  ## Credits
-  
-  
+  //license
   ${renderLicenseSection(data.license)}
 
-
   
-
-  
-  ## How to Contribute
+  ## Contribute
   
   ${data.contributors}
   
--- README.md
+++ README.md
@@ -1,19 +1,5 @@
 # README.GEN
 
-
-
-WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
-
-THEN this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
-
-WHEN I choose a license for my application from a list of options
-
-WHEN I enter my GitHub username
-THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
-
-WHEN I enter my email address
-THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
-
 WHEN I click on the links in the Table of Contents
 THEN I am taken to the corresponding section of the README
 
